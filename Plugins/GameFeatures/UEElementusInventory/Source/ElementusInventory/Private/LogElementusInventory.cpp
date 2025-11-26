fa6af184e940fd76b94af031766e573ca2e8063f
Anderson <andersonfilho09@gmail.com>
2025-11-26 18:10:42 -0300

    refactor(ElementusInventory): Remove SElementusSearch editor widget
    Removes the SElementusSearch.h header file from the ElementusInventoryEditor module.

--- Plugins/GameFeatures/UEElementusInventory/Source/ElementusInventory/Private/LogElementusInventory.cpp
+++
@@ -1,8 +0,0 @@
-// Author: Lucas Vilas-Boas
-// Year: 2023
-// Repo: https://github.com/lucoiso/UEElementusInventory
-
-#include "LogElementusInventory.h"
-
-DEFINE_LOG_CATEGORY(LogElementusInventory);
-DEFINE_LOG_CATEGORY(LogElementusInventory_Internal);
--- Plugins/GameFeatures/UEElementusInventory/Source/ElementusInventoryEditor/Private/ElementusStaticIds.h
+++
@@ -1,9 +0,0 @@
-// Author: Lucas Vilas-Boas
-// Year: 2023
-// Repo: https://github.com/lucoiso/UEElementusInventory
-
-#pragma once
-
-static const FName ElementusEditorTabId("Elementus Inventory");
-static const FName ItemCreatorTabId("Elementus Item Creator");
-static const FName ItemStackPropertyId("PrimaryElementusItemId");
