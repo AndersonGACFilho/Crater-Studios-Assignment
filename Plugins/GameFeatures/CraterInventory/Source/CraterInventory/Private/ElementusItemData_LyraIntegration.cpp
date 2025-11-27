3a48390bb13dd213946d0cf756b1f7e3b3b986ca
Anderson <andersonfilho09@gmail.com>
2025-11-27 17:29:40 -0300

    Revert "Merge pull request #13 from AndersonGACFilho/feature/inventory-core-manager"
    This reverts commit 9406ef0609220c01e6e735e08a472bde100dcaeb, reversing
    changes made to b1384a3ea592577a8bbb539500c4660961aacf6f.

--- Plugins/GameFeatures/CraterInventory/Source/CraterInventory/Private/ElementusItemData_LyraIntegration.cpp
+++
@@ -1,9 +0,0 @@
-#include "ElementusItemData_LyraIntegration.h"
-
-UElementusItemData_LyraIntegration::UElementusItemData_LyraIntegration(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-	// Default to 'Other' or 'Special' to indicate this relies on external Lyra Definitions 
-	// for its primary gameplay tags/behavior.
-	ItemType = EElementusItemType::Other;
-}
